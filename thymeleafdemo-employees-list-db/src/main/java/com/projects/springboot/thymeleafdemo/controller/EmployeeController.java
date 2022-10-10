e70eb505024e504f486c480abfb1657c0785255e
Alex <alexandrulapshin@gmail.com>
2022-10-10 16:04:22 -0400

    Update Employee Controller
    Update Controller to get Employees from EmployeeService(and eventually
    from a local database) instead of inMemory

--- thymeleafdemo-employees-list-db/src/main/java/com/projects/springboot/thymeleafdemo/controller/EmployeeController.java
+++ thymeleafdemo-employees-list-db/src/main/java/com/projects/springboot/thymeleafdemo/controller/EmployeeController.java
@@ -1,44 +1,34 @@
 package com.projects.springboot.thymeleafdemo.controller;
 
-import java.util.ArrayList;
 import java.util.List;
 
-import javax.annotation.PostConstruct;
-
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.projects.springboot.thymeleafdemo.entity.Employee;
+import com.projects.springboot.thymeleafdemo.service.EmployeeService;
 
 @Controller
 @RequestMapping("/employees")
 public class EmployeeController {
-
-	// load employee data
-	private List<Employee> theEmployees;
 	
-	@PostConstruct
-	private void loadData() {
-		// create employees
-		Employee emp1 = new Employee(1, "Leslie", "Andrews", "leslie@gmail.com");
-		Employee emp2 = new Employee(1, "Emma", "Doe", "emma@gmail.com");
-		Employee emp3 = new Employee(1, "Avani", "Gupta", "avani@gmail.com");
-		
-		// create the list
-		theEmployees = new ArrayList<>();
-		
-		// add to the list
-		theEmployees.add(emp1);
-		theEmployees.add(emp2);
-		theEmployees.add(emp3);
+	private EmployeeService employeeService;
 	
+	@Autowired
+	public EmployeeController(EmployeeService theEmployeeService) {
+		employeeService = theEmployeeService;
 	}
 	
 	// add mapping for "/list"
 	@GetMapping("/list")
 	public String listEmployees(Model theModel) {
+		
+		// get the employees from database
+		List<Employee> theEmployees = employeeService.findAll();
+		
 		// add to spring model
 		theModel.addAttribute("employees", theEmployees);
 		
